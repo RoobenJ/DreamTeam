8efdf0c236505d28d3d174cfafcdb56f41520b11
Pontus Nilsson <iceroad@neradio.com>
2017-01-26 09:30:29 +0100

    Skapat tabellen customers och lagt i mappen database

---
+++ database/customers.sql
@@ -0,0 +1,33 @@
+CREATE TABLE customers (
+	id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	regno VARCHAR(255),
+	address VARCHAR(255) NOT NULL,
+	zipcode VARCHAR(255) NOT NULL,
+	city VARCHAR(255) NOT NULL,
+	phoneno VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	secno VARCHAR(255) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (1, 'Lawrence Carpenter', null, '55523 Paget Court', '26654', 'Sverige', '63-(204)178-4787', 'lcarpenter0@blogtalkradio.com', '368-31-2189');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (2, 'Ryan Lewis', null, '606 Hazelcrest Avenue', '26654', 'Sverige', '352-(163)793-9267', 'rlewis1@youku.com', '946-24-3537');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (3, 'Irene Turner', null, '099 Victoria Crossing', '26654', 'Sverige', '33-(223)234-4250', 'iturner2@zdnet.com', '957-37-1423');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (4, 'Douglas Duncan', null, '63985 Rowland Road', '26654', 'Sverige', '218-(214)806-5619', 'dduncan3@odnoklassniki.ru', '234-07-5443');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (5, 'Laura Barnes', null, '5980 Ramsey Crossing', '26654', 'Sverige', '48-(798)154-3124', 'lbarnes4@mapy.cz', '994-46-2453');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (6, 'Sarah Hamilton', null, '36 Haas Avenue', '26654', 'Sverige', '81-(929)512-2374', 'shamilton5@cafepress.com', '461-64-2672');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (7, 'Samuel Hernandez', null, '4432 Northland Street', '26654', 'Sverige', '7-(526)327-7218', 'shernandez6@illinois.edu', '153-15-0130');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (8, 'Brandon Wright', null, '29 Ilene Pass', '26654', 'Sverige', '960-(805)887-4759', 'bwright7@sohu.com', '615-26-9642');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (9, 'Howard Little', null, '82998 Tomscot Plaza', '26654', 'Sverige', '84-(927)470-0614', 'hlittle8@elegantthemes.com', '507-24-5223');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (10, 'Bobby Rose', null, '89 Magdeline Plaza', '26654', 'Sverige', '86-(865)308-5178', 'brose9@bizjournals.com', '844-67-1842');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (11, 'Carl Austin', null, '047 Carberry Place', '26654', 'Sverige', '7-(639)858-3685', 'caustina@rakuten.co.jp', '308-83-6429');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (12, 'Douglas Martinez', null, '7 Esker Drive', '26654', 'Sverige', '81-(233)648-5112', 'dmartinezb@psu.edu', '289-09-9576');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (13, 'George Carter', null, '1899 Forest Dale Junction', '26654', 'Sverige', '52-(671)484-6276', 'gcarterc@ftc.gov', '565-45-6915');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (14, 'Paula Garrett', null, '105 Ryan Terrace', '26654', 'Sverige', '62-(203)533-3520', 'pgarrettd@fda.gov', '176-54-1049');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (15, 'Brandon Mason', null, '3420 Knutson Circle', '26654', 'Sverige', '32-(903)953-2034', 'bmasone@barnesandnoble.com', '269-57-3332');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (16, 'Sarah Knight', null, '607 Vermont Way', '26654', 'Sverige', '52-(930)176-1460', 'sknightf@stanford.edu', '488-33-4415');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (17, 'Virginia Torres', null, '02306 Manufacturers Road', '26654', 'Sverige', '7-(832)994-2387', 'vtorresg@google.fr', '475-45-5464');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (18, 'Carl Walker', null, '7 Mcbride Pass', '26654', 'Sverige', '423-(193)710-7362', 'cwalkerh@google.com.au', '910-78-0354');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (19, 'Janet Bryant', null, '120 Bunting Street', '26654', 'Sverige', '269-(706)866-8264', 'jbryanti@thetimes.co.uk', '519-07-8570');
+insert into customers (id, name, regno, address, zipcode, city, phoneno, email, secno) values (20, 'Bonnie Myers', null, '918 Kenwood Terrace', '26654', 'Sverige', '62-(418)345-1683', 'bmyersj@purevolume.com', '310-59-6716');
