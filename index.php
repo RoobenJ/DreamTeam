dddb29569ec02a6b44ca19b73e33ac255cf68776
Pontus Nilsson <iceroad@neradio.com>
2017-01-26 09:09:31 +0100

    Lagt in Jquery

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+	<!-- Charset -->
+	<meta charset="UTF-8">
+	<title>Dreamteam workshop</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- JAVSCRIPT LIBS -->
+	<script src="js/libs/jquery-3.1.1.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
