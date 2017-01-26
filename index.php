7c2fc0f6240a64d45f0d7cb504507a6df5aa324f
Pontus Nilsson <iceroad@neradio.com>
2017-01-26 14:15:05 +0100

    Lagt in bootstrap

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 	<title>Dreamteam workshop</title>
 
 	<!-- CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 
 	<!-- JAVSCRIPT LIBS -->
 	<script src="js/libs/jquery-3.1.1.min.js"></script>
+	<script src="js/libs/bootstrap.min"></script>
 </head>
 <body>
 	
