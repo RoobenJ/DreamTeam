ff6c00247e54597aa73ba40fec41385a75aaa058
Cia Alnerheim <alnerheim@yahoo.se>
2017-01-27 13:55:10 +0100

    tabellerna empXlog, empXvehicles och status klara och inlagda i mapp database

---
+++ database/employeesxvehicles.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 27 jan 2017 kl 13:49
+-- Serverversion: 10.1.16-MariaDB
+-- PHP-version: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `dreamteam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `employeesxvehicles`
+--
+
+CREATE TABLE `employeesxvehicles` (
+  `eID` int(11) NOT NULL,
+  `vID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
