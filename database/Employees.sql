5d647fce7ec1ef56122e50f07e0e1c9147d1be3f
Leen Lavin <leenbutterfly77@yahoo.com>
2017-01-27 13:37:53 +0100

    fixat employees tabel

--- database/Employees.sql
+++ database/Employees.sql
@@ -1,50 +1,121 @@
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('47Tf6l', 'Lawrence Lewis', '58 Burning Wood Crossing', 'Male', 'Totoras', 'llewis0@netscape.com', '54-(439)107-8218');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('DFq2vAP', 'Eric Fowler', '87 Walton Avenue', 'Male', 'Września', 'efowler1@eepurl.com', '48-(560)582-7627');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('KWJ7cwfMI', 'Helen Matthews', '3 Lotheville Court', 'Female', 'Tijucas', 'hmatthews2@japanpost.jp', '55-(138)185-6978');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('DoxEEE7u', 'Alan Wood', '73806 Ilene Park', 'Male', 'Pombos', 'awood3@arizona.edu', '55-(795)856-8671');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('ohf8pb', 'Benjamin Moreno', '75144 Bellgrove Park', 'Male', 'Qiaojia', 'bmoreno4@shareasale.com', '86-(208)160-8961');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('fJgarXBO', 'Doris Martin', '4 Little Fleur Parkway', 'Female', 'General Manuel J. Campos', 'dmartin5@homestead.com', '54-(828)352-7362');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('8sWghdu9K3F', 'Lori Reed', '402 7th Way', 'Female', 'Ziway', 'lreed6@diigo.com', '251-(193)981-4531');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('pgeEDxTYfK', 'Robert Lawrence', '641 Eastlawn Place', 'Male', 'Cerrito', 'rlawrence7@prnewswire.com', '57-(354)631-1962');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('AeM6KN', 'Melissa Allen', '8880 Charing Cross Lane', 'Female', 'Jingxi', 'mallen8@usda.gov', '86-(672)958-0519');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('Aqg2uL', 'Doris Kim', '15032 Manley Parkway', 'Female', 'Huaidian', 'dkim9@columbia.edu', '86-(861)564-4960');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('Wu3NTaDK1S65', 'Harold Cole', '07 Havey Plaza', 'Male', 'Peterborough', 'hcolea@nature.com', '1-(137)877-6762');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('twxNgWEkSPl', 'Matthew Sims', '86060 Valley Edge Junction', 'Male', 'Novaya Derevnya', 'msimsb@whitehouse.gov', '7-(514)246-7134');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('6CTEcURVFJ3P', 'Edward Rogers', '55 Brown Parkway', 'Male', 'Jintang', 'erogersc@naver.com', '86-(818)613-7139');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('3rJFXu3tm', 'Stephanie Hart', '19 Butternut Way', 'Female', 'Rizal', 'shartd@craigslist.org', '63-(575)400-0731');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('zVQI3v2', 'Joan Hart', '842 Brickson Park Place', 'Female', 'Barili', 'jharte@google.es', '63-(502)240-8928');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('LrERZ6', 'Sandra Green', '1665 Mayer Court', 'Female', 'Az Zarbah', 'sgreenf@abc.net.au', '963-(680)688-5796');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('z8NVNqbD', 'Clarence Simpson', '25022 Meadow Vale Court', 'Male', 'Zhongying', 'csimpsong@tripadvisor.com', '86-(622)485-4339');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('mZ2lAA8VAcgE', 'Howard Sims', '719 6th Junction', 'Male', 'Stepnogorsk', 'hsimsh@dion.ne.jp', '7-(894)113-2876');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('elSEn3q76xT9', 'Julie Kelly', '8171 Farmco Parkway', 'Female', 'Maogang', 'jkellyi@dot.gov', '86-(923)138-3658');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('ZjtRLeUY4GJ', 'Stephen Jones', '41822 Memorial Lane', 'Male', 'Yalagüina', 'sjonesj@whitehouse.gov', '505-(379)448-4450');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('5FaAXJJAN9Vx', 'Craig Harris', '123 Beilfuss Terrace', 'Male', 'Acandí', 'charrisk@ustream.tv', '57-(208)432-9721');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('MDkzdI', 'Elizabeth Hernandez', '1 4th Terrace', 'Female', 'Pulau Pinang', 'ehernandezl@hatena.ne.jp', '60-(200)555-7454');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('stAIJuchE9', 'Eric Little', '16226 Butternut Crossing', 'Male', 'Erdaohe', 'elittlem@un.org', '86-(327)753-4650');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('fEjwCN8ARht', 'Jesse Lewis', '7844 Mifflin Terrace', 'Male', 'Solna', 'jlewisn@foxnews.com', '46-(959)545-2416');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('Woc7ENBeB', 'Dennis Simpson', '47 Hoard Pass', 'Male', 'Kota Bharu', 'dsimpsono@sciencedaily.com', '60-(429)642-4403');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('6NFA2x8j', 'Louise Hanson', '3 Sunfield Lane', 'Female', 'Puger', 'lhansonp@multiply.com', '62-(697)154-7184');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('ahq40Mi', 'Roger Gordon', '73 Superior Point', 'Male', 'Gonça', 'rgordonq@gnu.org', '351-(410)436-4138');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('6Roykb8r', 'Terry Henderson', '0 Wayridge Junction', 'Male', 'Johanneshov', 'thendersonr@yale.edu', '46-(651)877-7412');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('jRgPnneJ9cu', 'Alice Oliver', '4 Eagan Pass', 'Female', 'Bayan Huxu', 'aolivers@deviantart.com', '86-(656)734-4683');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('343djMtu', 'Walter Evans', '8 Mariners Cove Street', 'Male', 'Ţahţā', 'wevanst@studiopress.com', '20-(700)477-4019');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('kYy0K6', 'Carol Young', '2935 Gale Avenue', 'Female', 'Saga', 'cyoungu@hatena.ne.jp', '86-(492)854-2572');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('K62e7j0f1khu', 'Ruth Barnes', '8606 Graceland Trail', 'Female', 'Maliang', 'rbarnesv@chron.com', '86-(610)782-8391');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('CTCWLeuj', 'Dorothy Lane', '68 Straubel Alley', 'Female', 'Ivatsevichy', 'dlanew@linkedin.com', '375-(191)497-2012');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('5OXo4I2kf', 'Benjamin Black', '43336 Mcbride Road', 'Male', 'Taoxi', 'bblackx@sciencedaily.com', '86-(166)781-0550');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('4UYpGh3Q2', 'Billy Butler', '07 Wayridge Court', 'Male', 'Vatutino', 'bbutlery@bigcartel.com', '7-(619)473-5394');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('lnIOePAc1', 'Judith Reid', '5510 2nd Alley', 'Female', 'Maradabangga', 'jreidz@topsy.com', '62-(254)468-6397');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('YKWtXn', 'Carlos Davis', '5324 Ronald Regan Place', 'Male', 'Kalāt', 'cdavis10@canalblog.com', '92-(175)179-9667');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('XZa8DP', 'Paula Johnston', '922 Gina Crossing', 'Female', 'Breia', 'pjohnston11@posterous.com', '351-(821)280-3742');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('CHsfLCRnQAnn', 'Shirley Green', '2028 Lien Parkway', 'Female', 'San Juan de Mata', 'sgreen12@networkadvertising.org', '63-(974)513-0119');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('HOVDlSFE', 'Annie Hall', '9006 Waywood Way', 'Female', 'Pierreville', 'ahall13@discovery.com', '1-(743)249-4526');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('MgjVhCGOsL', 'Walter Perez', '05 Gerald Street', 'Male', 'Qiryat Motzkin', 'wperez14@disqus.com', '972-(695)943-3469');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('2UhM8Qg5THs', 'Rebecca Bennett', '7005 South Lane', 'Female', 'Mamonovo', 'rbennett15@github.com', '7-(639)212-3638');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('S7pYGvjOOzT', 'John Harris', '9535 Acker Court', 'Male', 'Wanzhai', 'jharris16@jalbum.net', '86-(546)526-2963');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('8snM9hg', 'Harry Evans', '2 Hermina Terrace', 'Male', 'Tembilahan', 'hevans17@nba.com', '62-(520)912-8846');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('jENmdeNst', 'Gary Dixon', '5469 Bartelt Point', 'Male', 'Podgorica', 'gdixon18@telegraph.co.uk', '382-(519)923-4951');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('IgwDLAG7', 'Harry Butler', '198 Pond Place', 'Male', 'Qianjiang', 'hbutler19@thetimes.co.uk', '86-(377)413-7118');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('2NU8LKdkjimR', 'Harold Payne', '6 Lakeland Plaza', 'Male', 'Grande Rivière Noire', 'hpayne1a@networkadvertising.org', '230-(845)214-3946');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('lyE6AnZLbS', 'Donna Flores', '1136 Brickson Park Street', 'Female', 'Malusac', 'dflores1b@phoca.cz', '63-(518)606-9700');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('u8tAi32j', 'Daniel Freeman', '158 Gateway Drive', 'Male', 'Stony Hill', 'dfreeman1c@lulu.com', '1-(399)223-1724');
-insert into Employees (password, name, address, zipCode, city, email, phoneNo) values ('H2gdg6', 'Nancy Martin', '243 Twin Pines Avenue', 'Female', 'Ukata', 'nmartin1d@seattletimes.com', '234-(317)225-3383');
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 27, 2017 at 01:29 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dreamwork`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+-- Creation: Jan 26, 2017 at 06:07 PM
+--
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
+  `password` varchar(12) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zipCode` varchar(5) DEFAULT NULL,
+  `city` varchar(255) NOT NULL,
+  `phoneNo` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `password`, `name`, `address`, `zipCode`, `city`, `phoneNo`, `email`, `role`) VALUES
+(101, 'OcSD1PJg9T', 'Tina Evans', '94 Grim Court', '22365', 'Fushui', '', 'tevans0@baidu.com', 0),
+(102, 'rVaOweeZPd3i', 'Alice Wallace', '39665 Aberg Junction', '22365', 'Ibaiti', '', 'awallace1@myspace.com', 0),
+(103, 'jGpwFN', 'Shawn Kennedy', '665 Bayside Pass', '22365', 'Bāniyās', '', 'skennedy2@sciencedirect.com', 0),
+(104, 'RK16cbGU82b3', 'Randy Fowler', '0 Oak Road', '22365', 'Marfino', '', 'rfowler3@google.com', 0),
+(105, 'OXWBXnCcDD02', 'Diane Perez', '497 Grasskamp Way', '22365', 'Mailag', '', 'dperez4@ebay.co.uk', 0),
+(106, 'Ys4Zl2sctt', 'Heather Perry', '2973 Crest Line Junction', '22365', 'Eusébio', '', 'hperry5@bluehost.com', 0),
+(107, 'Zhg2sWjFNPp', 'Jack Wilson', '27367 South Place', '22365', 'Bartolomé Masó', '', 'jwilson6@soundcloud.com', 0),
+(108, 'CnZXZlp', 'Willie Richards', '01889 Nobel Road', '22365', 'Rovira', '', 'wrichards7@livejournal.com', 1),
+(109, 'tyMmQ2sA', 'Charles Lopez', '581 Ronald Regan Pass', '22365', 'Cipanggung', '', 'clopez8@vkontakte.ru', 0),
+(110, 'gZ82aqr3u', 'Ronald Gordon', '66681 Grim Court', '22365', 'Yambio', '', 'rgordon9@skyrock.com', 0),
+(111, 'Mk6lXRtQiQoG', 'Richard Bradley', '0266 Riverside Hill', '22365', 'Cikayas', '', 'rbradleya@yellowbook.com', 0),
+(112, 'o5qHmbsLQ', 'Jimmy Chapman', '4136 Derek Center', '22365', 'Nuwara Eliya', '', 'jchapmanb@webs.com', 0),
+(113, 'i24vZUW', 'Fred Schmidt', '273 Mosinee Place', '22365', 'Velké Karlovice', '', 'fschmidtc@1und1.de', 0),
+(114, 'wTRbbp7R4J', 'Dennis Wheeler', '7 Hayes Place', '22365', 'Bologna', '', 'dwheelerd@mozilla.com', 0),
+(115, '3b1gfMpy', 'Bobby Snyder', '133 Sycamore Plaza', '22365', 'Dubréka', '', 'bsnydere@princeton.edu', 0),
+(116, 'RrxOFt', 'Nancy Cooper', '69420 Chive Way', '22365', 'Rudnichnyy', '', 'ncooperf@ebay.co.uk', 0),
+(117, '3OMMexE', 'Sharon Payne', '5 Kinsman Court', '22365', 'Bata Tengah', '', 'spayneg@bloglovin.com', 0),
+(118, 'LLtzUuO34g', 'Gerald Wallace', '47370 Becker Hill', '22365', 'Jiangjia', '', 'gwallaceh@sfgate.com', 0),
+(119, 'FsPiLWL1', 'Elizabeth Cruz', '99 Nobel Circle', '22365', 'Shuangpengxi', '', 'ecruzi@bigcartel.com', 0),
+(120, 'ETIyYTNEgp', 'Dennis Bailey', '0625 Northland Circle', '22365', 'Cornillon', '', 'dbaileyj@google.nl', 0),
+(121, 'gbRGQv', 'Howard Arnold', '66113 Hagan Crossing', '22365', 'Luleå', '', 'harnoldk@livejournal.com', 0),
+(122, 'ydZNq3wm0YI', 'Gloria Franklin', '4846 Crescent Oaks Center', '22365', 'Santa Maria', '', 'gfranklinl@tinyurl.com', 1),
+(123, '7AYMG4vdX', 'Ralph Garcia', '33 Porter Junction', '22365', 'Adil Makmur', '', 'rgarciam@slashdot.org', 0),
+(124, '8cVcmfcPY', 'Jeremy Lopez', '749 David Circle', '22365', 'Dampek', '', 'jlopezn@bandcamp.com', 0),
+(125, 'zWgbuKEKF', 'Nicholas Chavez', '1011 Bunting Street', '22365', 'Czechowice-Dziedzice', '', 'nchavezo@arizona.edu', 0),
+(126, '3UDAtoe', 'Stephen Peterson', '6 Kenwood Trail', '22365', 'Santa Elena', '', 'spetersonp@webmd.com', 0),
+(127, 'v1kfX8up2U', 'Andrew Hall', '97 Manitowish Parkway', '22365', 'Triolet', '', 'ahallq@amazon.co.jp', 0),
+(128, 'mHM5YAMn', 'Margaret Chavez', '98677 Scofield Pass', '22365', 'Heqing', '', 'mchavezr@goo.gl', 0),
+(129, 'NXp4Hl', 'Martin Fowler', '6849 Algoma Terrace', '22365', 'Villeneuve-lès-Avignon', '', 'mfowlers@fema.gov', 0),
+(130, 'TC0nxE8Kf', 'Tammy Ferguson', '37239 Pearson Point', '22365', 'Toupi', '', 'tfergusont@gnu.org', 0),
+(131, '2WdfztvBt', 'Russell Mendoza', '282 Washington Crossing', '22365', 'Cunday', '', 'rmendozau@php.net', 0),
+(132, 'yfSC1QUi', 'Doris Ramirez', '26 Ridge Oak Way', '22365', 'Zaria', '', 'dramirezv@webs.com', 0),
+(133, 'axbFOFp2gPo', 'Melissa Cunningham', '11 Fuller Crossing', '22365', 'Tramandaí', '', 'mcunninghamw@sfgate.com', 0),
+(134, 'neOFwDD', 'Kelly Marshall', '985 Clemons Way', '22365', 'Pittsburgh', '', 'kmarshallx@dailymail.co.uk', 1),
+(135, 'Qh4SzR9', 'Eugene Hawkins', '0068 Mallory Pass', '22365', 'Xiangtan', '', 'ehawkinsy@pbs.org', 0),
+(136, '0JY3vlclg', 'Gerald Hayes', '392 Mayfield Way', '22365', 'Thành Phố Uông Bí', '', 'ghayesz@oaic.gov.au', 0),
+(137, 'xtEvr1B3t5', 'Ashley Mcdonald', '5 West Parkway', '22365', 'Tadine', '', 'amcdonald10@devhub.com', 0),
+(138, '7KO9D7APzyVz', 'John Hawkins', '4478 Ridgeview Lane', '22365', 'Chrysó', '', 'jhawkins11@engadget.com', 0),
+(139, 'cEFW6qJHyZU', 'Robert Price', '5243 Coleman Crossing', '22365', 'Muricay', '', 'rprice12@artisteer.com', 0),
+(140, 'g5EnZZcaUzO', 'Ernest Lopez', '9 Goodland Plaza', '22365', 'Sotteville-lès-Rouen', '', 'elopez13@lulu.com', 0),
+(141, 'hoothDU0Uo', 'Stephanie Diaz', '658 1st Avenue', '22365', 'Piribebuy', '', 'sdiaz14@reuters.com', 0),
+(142, 'WMyM6wducL', 'Louis Fowler', '774 Moose Plaza', '22365', 'Leshukonskoye', '', 'lfowler15@ehow.com', 0),
+(143, 'g2l9aH', 'Adam Lawson', '54099 Manitowish Parkway', '22365', 'Kultuk', '', 'alawson16@google.com', 0),
+(144, 'tSK8QDks', 'Elizabeth Stone', '0 Northwestern Hill', '22365', 'Cibingbin', '', 'estone17@wordpress.com', 0),
+(145, 'IMMxKG', 'Jane Gibson', '6294 Arrowood Avenue', '22365', 'Shirokovskiy', '', 'jgibson18@examiner.com', 0),
+(146, 'oHZpSD8JlMaN', 'Janet Gomez', '93 Browning Center', '22365', 'Novo Selo', '', 'jgomez19@nytimes.com', 0),
+(147, 'DHUa7L', 'Gerald Fowler', '13 Vera Hill', '22365', 'Bayombong', '', 'gfowler1a@nsw.gov.au', 0),
+(148, 'gpW0yM', 'Kathleen Riley', '849 Pond Court', '22365', 'Uvira', '', 'kriley1b@auda.org.au', 1),
+(149, 'dGNrr9efTV', 'William Cruz', '274 Calypso Terrace', '22365', 'El Tabo', '', 'wcruz1c@examiner.com', 0),
+(150, '5ydV5aDWQh8f', 'Richard Moore', '574 Fieldstone Alley', '22365', 'Potou', '', 'rmoore1d@furl.net', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=151;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
