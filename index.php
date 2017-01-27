790530056ea08338e03c7ed10ccb9372b8667d26
Pontus Nilsson <iceroad@neradio.com>
2017-01-27 14:17:00 +0100

    Lagt in en början på index. Gjort registrering av bil färdig. En liten meny finns på plats om vi kan använda den

--- index.php
+++ index.php
@@ -5,15 +5,38 @@
 	<meta charset="UTF-8">
 	<title>Dreamteam workshop</title>
 
-	<!-- CSS -->
+	<!-- CSS libs -->
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<!-- Own styles -->
 	<link rel="stylesheet" href="css/style.css">
 
 	<!-- JAVSCRIPT LIBS -->
 	<script src="js/libs/jquery-3.1.1.min.js"></script>
-	<script src="js/libs/bootstrap.min"></script>
+	<script src="js/libs/bootstrap.min.js"></script>
 </head>
 <body>
+<div class="container">
+	<div class="row">
+		<div class="col-xs-9">
+			<!-- INNEHÅLL FÖR EN SIDA -->
+		</div>
+
+		<!-- MENYN -->
+		<div class="col-xs-3 column margintop20">
+			<ul class="nav nav-pills nav-stacked">
+				<li class="active"><a href="#"><span class="glyphicon glyphicon-chevron-right"></span> Home</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span> Option 1</a></li>
+				<li><a href="#" class="active2"><span class="glyphicon glyphicon-chevron-right"></span> Option 2 (active)</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span> Option 3</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span> Option 4</a></li>
+				<li><a href="#"><span class="glyphicon glyphicon-chevron-right"></span> Option 5</a></li>
+			</ul>
+		</div>
+
+	</div>
+</div>
+
 	
 </body>
 </html>
